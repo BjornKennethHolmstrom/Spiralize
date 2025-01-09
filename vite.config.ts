a8e790dde9f318849c04081ca0fa8d7a9a3f74a2
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-01-09 20:57:34 +0100

    Modifying config files to be more explicit about the base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-  base: '/Spiralize/', 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  base: process.env.NODE_ENV === 'production' 
+    ? 'https://www.spiralize.org/Spiralize/'
+    : '',
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
