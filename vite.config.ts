33c5abc7dae57081518874424007a6ff12ca4cf0
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-01-09 08:47:47 +0100

    Add /Spiralize to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+  base: '/Spiralize/', 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
